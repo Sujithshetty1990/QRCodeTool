a5a7780dd914938eaf765c3baf1722f77b0c29a0
Sujithshetty1990 <sujsh@eg.dk>
2026-08-12 11:56:30 +0530

    Remove commented line in MMQRCodeGenerator.vb

--- QRCode/QRCode/MMQRCodeGenerator.vb
+++ QRCode/QRCode/MMQRCodeGenerator.vb
@@ -14,7 +14,7 @@ Public Class MMQRCodeGenerator
 		Dim qrColor1 = ColorDialog1.Color
 		Dim textColor = ColorDialog3.Color
 		Dim qrColor2 = Color.White
-
+		'Dim
 		If ColorDialog2.Color <> Color.White AndAlso ColorDialog2.Color <> Color.Black Then
 			qrColor2 = ColorDialog2.Color
 		End If
