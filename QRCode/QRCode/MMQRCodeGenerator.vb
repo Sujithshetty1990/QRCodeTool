96fbd9d47d63d7e45f19964614081cd8e2516db1
Ravithejaswi <ravrx@eg.dk>
2026-08-18 17:30:30 +0530

    Add qrColor9 for additional QR code customization

--- QRCode/QRCode/MMQRCodeGenerator.vb
+++ QRCode/QRCode/MMQRCodeGenerator.vb
@@ -14,6 +14,7 @@ Public Class MMQRCodeGenerator
 		Dim qrColor1 = ColorDialog1.Color
 		Dim textColor = ColorDialog3.Color
 		Dim qrColor2 = Color.White
+		Dim qrColor9 = Color.GoldenBrown
 
 		If ColorDialog2.Color <> Color.White AndAlso ColorDialog2.Color <> Color.Black Then
 			qrColor2 = ColorDialog2.Color
