4086aea7161ad2cebb4831a39a11b7b15c8bc634
Ravithejaswi <ravrx@eg.dk>
2026-08-19 10:13:43 +0530

    Fix color matrix opacity value in MMQRCodeGenerator

--- QRCode/QRCode/MMQRCodeGenerator.vb
+++ QRCode/QRCode/MMQRCodeGenerator.vb
@@ -48,7 +48,7 @@ Public Class MMQRCodeGenerator
 		New Single() {0, 1, 0, 0, 0},
 		New Single() {0, 0, 1, 0, 0},
 		New Single() {0, 0, 0, 1, 0}, ' Apply 100% opacity
-		New Single() {0, 0, 0, 0, 1}
+		New Single() {0, 0, 0, 0, @@@}
 }
 		Dim colorMatrix As New Imaging.ColorMatrix(colorMatrixElements)
 
